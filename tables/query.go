22e5b5f481721c9b82c74fdf477709f95c6562b4
Arvind Parthasarathy <arvindpadev@gmail.com>
2022-12-11 21:52:57 -0600

    fix: ConsistentQueryShopByStream

--- tables/query.go
+++ tables/query.go
@@ -3,9 +3,11 @@ package tables
 import (
 	"context"
 	"fmt"
+	"log"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/attributevalue"
 	"github.com/aws/aws-sdk-go-v2/feature/dynamodb/expression"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 )
 
 func QueryAllInstancesWithNumStreams(ctx context.Context, ddb *dynamodb.Client, num uint8) (*[]InstanceNameType, error) {
@@ -66,5 +68,38 @@ func QueryInstancesUsingPort(ctx context.Context, ddb *dynamodb.Client, port uin
 }
 
 func ConsistentQueryShopByStream(ctx context.Context, ddb *dynamodb.Client, stream string) (*ShopType, error) {
-	return nil, fmt.Errorf("Not Implemented")
+	kexpr := expression.Key(*Shops.Stream.AttributeName).Equal(expression.Value(stream))
+	expr, err := expression.NewBuilder().WithKeyCondition(kexpr).Build()
+	if err != nil {
+		return nil, fmt.Errorf("Unable to create expression for query [%v]", err)
+	}
+
+	input := dynamodb.QueryInput {
+		TableName: Shops.TableName,
+		IndexName: &ShopsGsiStream,
+		Select: types.SelectAllAttributes,
+		ExpressionAttributeNames: expr.Names(),
+		ExpressionAttributeValues: expr.Values(),
+		KeyConditionExpression: expr.KeyCondition(),
+	}
+
+	output, err := ddb.Query(ctx, &input)
+	if err != nil {
+		return nil, fmt.Errorf("Could not query Shops table with stream %v [%v]", stream, err)
+	}
+
+	if len(output.Items) == 0 {
+		return nil, fmt.Errorf("No shop record found for stream %s", stream)
+	} else if len(output.Items) > 1 {
+		// don't panic, since we aren't adding to our data corruption problem here
+		log.Println(fmt.Sprintf("ERROR: More than one shop for stream %s was detected"))
+	}
+
+	var records []ShopType
+	err = attributevalue.UnmarshalListOfMaps(output.Items, &records)
+	if err != nil {
+		return nil, err
+	}
+
+	return &records[0], nil
 }
